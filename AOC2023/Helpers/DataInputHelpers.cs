3d20881f58fd975e8b1716e4fb9d4a5b71a16eb2
noxydee <37812367+noxydee@users.noreply.github.com>
2023-12-03 14:22:16 +0100

    Added AOC03

---
+++ AOC2023/Helpers/DataInputHelpers.cs
@@ -0,0 +1,26 @@
+﻿namespace AOC2023.Helpers
+{
+    public static class DataInputHelpers
+    {
+        public static async Task<List<string>> GetInputLinesAsync(string wholeInput)
+        {
+            List<string> inputLines = new List<string>();
+            
+            StringReader reader = new(wholeInput);
+
+            while (true)
+            {
+                string? line = await reader.ReadLineAsync();
+
+                if (line == null)
+                {
+                    break;
+                }
+
+                inputLines.Add(line);
+            }
+
+            return inputLines;
+        }
+    }
+}
